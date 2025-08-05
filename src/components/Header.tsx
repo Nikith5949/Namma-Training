538d659f16ae925738c8365e0c096c87bae8b623
Nikith <nikithanupkumar@gmail.com>
2025-08-05 13:32:35 +0530

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { NammaMyLogo } from "@/components/NammaMyLogo";
-type Props = {};
 
-export default function Header({}: Props) {
+export default function Header() {
   return (
     <header className="flex justify-center py-1">
-      {/* <NammaLogo className="z-10 h-20 cursor-pointer text-sky-800"/> */}
-
       <NammaMyLogo className="h-13 w-60 cursor-pointer py-1" />
     </header>
   );
