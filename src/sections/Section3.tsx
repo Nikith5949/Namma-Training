8bf9ff9ee324049c34969d75419ae2925f6a5e81
Nikith <nikithanupkumar@gmail.com>
2025-08-16 18:25:56 +0530

    cool

--- src/sections/Section3.tsx
+++ src/sections/Section3.tsx
@@ -68,8 +68,8 @@ export default function Section3() {
             </div>
           </div>
           {/* Bottom Spacer and Divider */}
-          <div className="h-[40vh]" />
-          <div className="h-0.5 w-[90vw] max-w-6xl mx-auto bg-[var(--theme-color)]" />
+          <div className="h-[40svh]" />
+          <div className="h-0.5 w-[90svw] max-w-6xl mx-auto bg-[var(--theme-color)]" />
         </div>
       </section>
     </>
